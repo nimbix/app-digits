6f0866f2f7cfa17b0d2a1bde1acb0ddcd04f98f8
Leo Reiter <leo.reiter@nimbix.net>
2017-03-19 16:34:20 -0500

    source profile scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # http://docs.gunicorn.org/en/stable/settings.html
+
+for i in /etc/profile.d/*; do
+    source $i
+done
 
 sudo service nginx start
 sudo service ssh start
@@ -19,7 +23,6 @@ mkdir -p ${DIGITS_JOBS_DIR}
 DIGITS_FORCE_SSL=1
 export DIGITS_FORCE_SSL
 
-cd /usr/share/digits
 python digits -p 34448
 
 #gunicorn --certfile /etc/JARVICE/cert.pem --keyfile /etc/JARVICE/cert.pem --config gunicorn_config.py digits.webapp:app 2>&1 | tee -a /var/log/digits/digits.log
